a5c0e3d1ecf4e3bb4f5fe5efb53acf9e7eb0751a
bealecs <cliftonscottbeale@gmail.com>
2023-10-18 13:09:52 -0500

    tweaks to copy and styles

--- app/About/About.tsx
+++ app/About/About.tsx
@@ -17,12 +17,12 @@ export const AboutPage = () => {
           />
           <p>
             <span className={AboutStyles.firstLetter}>L</span>earning web
-            development has been a passion of mine for over a year now, focusing
+            development has been a passion of mine for over two years now, focusing
             more on the front-end, but also exploring other aspects.
             <br />
             <br />
             <span className={AboutStyles.firstLetter}>M</span>y goal is to
-            improve each day with the help of{" "}
+            improve each day and learn something new all while attempting to land a new job. With the help of{" "}
             <a
               className={AboutStyles.vwcTag}
               href="https://vetswhocode.io"
@@ -32,8 +32,14 @@ export const AboutPage = () => {
               #VetsWhoCode
             </a>
             , a veteran-led community that trains and supports transitioning
-            veterans in the development field. I am grateful to be part of this
-            amazing group and excited to learn more in the future.
+            veterans in the development field, I am working alongside leaders in the industry to help me grow and succeed. Be sure to check out my{" "} 
+            <a className={AboutStyles.vwcTag}
+               href="/projects"
+               target="_blank"
+               rel="noopener noreferrer">
+                projects
+            </a>
+            {" "}to see a list of some of my work.
           </p>
           <br />
           <p>
@@ -58,20 +64,27 @@ export const AboutPage = () => {
           while building on my knowledge of web development.
           <br />
           <br />
-          <span className={AboutStyles.firstLetter}>W</span>ith my current
+          <span className={AboutStyles.firstLetter}>W</span>ith my
           cohort, we were all a part of the first online cohort for Vets Who
           Code. Throughout the program, I learned a lot about version control,
           working with others/asking for help, creating and consuming RESTful
           API data with NextJS/NodeJS/React/& TypeScript, and more.
           <br />
           <br />
-          <span className={AboutStyles.firstLetter}>C</span>ontinuously working
-          over the last few months, I have been completing the cohort and
-          finishing issues each week. From styling my basic static page with
-          HTML CSS and vanilla JS, to migrating now using NextJS/NodeJS/React/&
+          <span className={AboutStyles.firstLetter}>C</span>ompleting the cohort
+          was a very rewarding experience. From styling my basic static portfolio page with
+          HTML, CSS, and vanilla JS, to migrating now using Next.js/Node.js/React.js/&
           TypeScript. It was a bit bumpy along the way, but boy did I learn a
           lot. I cannot wait to apply what I have learned into the my future
-          work and continue building on my skills.
+          work and continue building on my skills. Please check out my{" "}
+          <a className={AboutStyles.vwcTag}
+             href="https://dev.to/bealecs"
+             target="_blank"
+             rel="noopener noreferrer"
+          >
+            blog posts
+          </a>
+          {" "} to read some of my blogs in regards to the process.
         </p>
       </section>
     </div>
